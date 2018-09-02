1006e6a54528222d0045cbb570d5b98c9fe42bf4
ketankokane94 <ketankokane94@gmail.com>
2018-09-02 19:47:15 -0400

    completd

--- CS CI 605 Java/Hw1/Prime.java
+++ CS CI 605 Java/Hw1/Prime.java
@@ -2,9 +2,9 @@
  * Prime.java
  * 
  * Version :
- *          1
+ *          1.0
  * Revisions : 
- *          1
+ *          1.0
  */
 
 
@@ -15,53 +15,63 @@
   * @author Ameya Deepak Nagnur
   */
 
-public class Prime {
-
+  public class Prime {
+    //Array to store all prime numbers from 2 to N
+    public static int[] primeNumbersUptoN = new int[4];
+    //Array to store all the prime factorials from index 1 and their sum at index 0
+    public static int[] result = new int[5];
+    
     /**
      * The main Program 
      * 
-     * @params  args     command Line arguments(ignored) 
+     * @param  args     command Line arguments(ignored) 
      */
 
     public static void main(String args[]) {
         int internalArrayIndex = 0;
-        for (int index = 2; index <= 10; index++)
-        {
+        
+        //Store number in array primeNumbersUptoN if it is prime
+        for (int index = 2; index <= 10; index++) {
             if (isPrime(index)) {
-                // print directly
-                //printResult(new int[]{index},index);
                 primeNumbersUptoN[internalArrayIndex] = index;
                 internalArrayIndex++;
             }
         }
 
-        {
-            for (int index = 2; index <= 10; index++) {
-                int N = index;
-                int i = 0, j = 1;
-                while (N > 1) {
-                    if (N % primeNumbersUptoN[i] == 0) {
-                        result[0] += primeNumbersUptoN[i];
-                        result[j] = primeNumbersUptoN[i];
-                        N = N / primeNumbersUptoN[i];
-                        j++;
-                        i = 0;
-                    } else {
-                        i++;
-                    }
-
+        //If prime number divides N then add it to result and check again from start for N/(the prime number)
+        for (int index = 2; index <= 10; index++) {
+            int N = index;
+            int primeNumbersIndex = 0, resultIndex = 1;
+            while (N > 1) {
+                if (N % primeNumbersUptoN[primeNumbersIndex] == 0) {
+                    //result at 0 stores sum of prime factorials
+                    result[0] += primeNumbersUptoN[primeNumbersIndex];
+                    //result from 1 to end of array stores all the prime factorials found
+                    result[resultIndex] = primeNumbersUptoN[primeNumbersIndex];
+                    //Divide N by the prime factorial found to find next prime factorial which can repeat
+                    N = N / primeNumbersUptoN[primeNumbersIndex];
+                    resultIndex++;
+                    primeNumbersIndex = 0;
                 }
-                printResult(result,index);
-                result = new int[5];
+                else {
+                    primeNumbersIndex++;
+                }
+
             }
+            //Print prime factorials of number index and their sum
+            printResult(result,index);
+            result = new int[5];
         }
-
-
 
     }
 
-    public static int[] primeNumbersUptoN = new int[4];
-    public static int[] result = new int[5];
+    /**
+     * Check if number n is prime or not by checking if it is divisible
+     * by any number from 2 to n - 1
+     *
+     * @param   n   number to check for prime
+     */
+    
     public static boolean isPrime(int n) {
 
         for (int index = 2; index < n; index++) {
@@ -71,15 +81,24 @@ public class Prime {
         return true;
     }
 
+    /**
+     * Print prime factorials of number index and their sum
+     * 
+     * @param     result    Array with prime factorials of index and their sum
+     * @param      index     Number for which the sum of prime factorials has been found
+     */
+    
     public static void printResult(int[] result, int index){
+        
+        //Print the result in the required format Eg. The sum of all primes for 4: 4    (2 + 2)
         System.out.print("The sum of all primes for "+ index+ ": "+result[0] + "  (");
-        for (int i = 1; i < result.length; i++) {
+        for (int resultIndex = 1; resultIndex < result.length; resultIndex++) {
             
-            if(result[i]!=0)
+            if(result[resultIndex]!=0)
             {
-                if(i > 1)
+                if(resultIndex > 1)
                     System.out.print(" + ");
-                System.out.print(result[i]);
+                System.out.print(result[resultIndex]);
             }
                 
             
