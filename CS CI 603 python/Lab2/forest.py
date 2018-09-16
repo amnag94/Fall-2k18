1ff43d281fae5274349530632fc768230b17f067
ketankokane94 <ketankokane94@gmail.com>
2018-09-16 14:44:30 -0400

    applied coding standard

--- CS CI 603 python/Lab2/forest.py
+++ CS CI 603 python/Lab2/forest.py
@@ -8,10 +8,12 @@ import turtle
 pre : (0,0), east, up
 pre : (0,length), north, up
 '''
-def drawTunk(length):
+
+
+def draw_trunk(trunk_length):
     t.down()
     t.left(90)
-    t.fd(length)
+    t.fd(trunk_length)
     t.up()
 
 
@@ -20,10 +22,12 @@ pre : (0,0), 120, up
 pre : (0,0), 120, up
 '''
 # todo: can be changed to draw polygon
-def drawTriangle(pineLength):
+
+
+def draw_triangle(side_length):
     t.down()
     for _ in range(3):
-        t.fd(pineLength)
+        t.fd(side_length)
         t.right(120)
     t.up()
 
@@ -31,42 +35,44 @@ def drawTriangle(pineLength):
 pre : (0,0), north, up
 pre : (0,length), north, up
 '''
-def drawPineTreeTop():
-    pineLength = 50
+
+
+def draw_pine_tree_top():
+    pine_leaf_length = 50
     t.left(90)
-    t.fd(pineLength // 2)
+    t.fd(pine_leaf_length // 2)
     t.right(120)
     t.up()
-    drawTriangle(pineLength)
+    draw_triangle(pine_leaf_length)
     t.right(60)
-    t.fd(pineLength // 2)
+    t.fd(pine_leaf_length // 2)
     t.left(90)
 
 '''
 pre : (0,length), north, up
 pre : (0,length), north, up
 '''
-def drawMapleTreeTop():
-    leafLength = 25
+
+
+def draw_maple_tree_top():
+    maple_leaf_length = 25
     t.down()
     t.right(90)
-    t.circle(leafLength)
+    t.circle(maple_leaf_length)
     t.left(90)
     t.up()
-
-
-
 
 '''
 pre : (0,length) , north, up
 
 '''
-def drawTreeTop(type,length):
-    if type == 'pine':
-        # pine tree always draws tree top of length 50 -25 on left and 25 on right
-        drawPineTreeTop()
-    elif type == 'maple':
-        drawMapleTreeTop()
+
+
+def draw_tree_top(tree_type):
+    if tree_type == 'pine':
+        draw_pine_tree_top()
+    elif tree_type == 'maple':
+        draw_maple_tree_top()
 
 
 
@@ -74,17 +80,19 @@ def drawTreeTop(type,length):
 pre : (0,0), east, up
 pre : (0,0), east, up
 '''
-def drawTree(length,type):
+
+
+def draw_tree(length, tree_type):
     t.up()
-    drawTunk(length)
-    drawTreeTop(type,length)
+    draw_trunk(length)
+    draw_tree_top(tree_type)
     t.bk(length)
     t.right(90)
     t.up()
     return length
 
 
-def drawBase():
+def draw_picture_base():
     t.speed(0)
     t.up()
     t.setpos(-290,-290)
@@ -99,15 +107,19 @@ def drawBase():
     t.speed(6)
 
 
-def setCordinates():
+def initialise_screen():
     turtle.setup(width=1200, height=800, startx=0, starty=0)
-    turtle.setworldcoordinates(-300,-300,300,300)
+    left_point = -400
+    right_point = 400
+    turtle.setworldcoordinates(left_point,left_point,right_point,right_point)
 
 
 '''
 pre pos are same
 '''
-def drawSquare(length):
+
+
+def draw_square(length):
     t.down()
     for _ in range(4):
         t.fd(length)
@@ -116,7 +128,7 @@ def drawSquare(length):
     t.up()
 
 
-def drawRoofTop(length):
+def draw_roof_top(length):
     t.left(45)
     t.down()
     t.fd(length)
@@ -124,42 +136,41 @@ def drawRoofTop(length):
     t.fd(length)
 
 
-
-def drawHouse(length):
-    drawSquare(length)
+def draw_house(wall_height):
+    draw_square(wall_height)
     t.left(90)
-    t.fd(length)
-    drawRoofTop(length/math.sqrt(2))
+    t.fd(wall_height)
+    draw_roof_top(wall_height / math.sqrt(2))
     t.up()
     t.left(45)
-    t.fd(length)
+    t.fd(wall_height)
     t.left(90)
-    t.fd(length)
+    t.fd(wall_height)
 
 
-def drawStar(height):
-    starlength = 10
+def draw_star(sky_height):
+    star_size = 10
     t.left(90)
-    t.fd(height)
+    t.fd(sky_height)
     t.down()
     for _ in range(4):
-        t.fd(starlength)
-        t.bk(starlength)
+        t.fd(star_size)
+        t.bk(star_size)
         t.right(90)
         t.up()
-        t.circle(starlength//2,45)
+        t.circle(star_size//2, 45)
         t.down()
         t.left(90)
     t.up()
-    t.fd(starlength)
+    t.fd(star_size)
     t.left(90)
 
 
 if __name__ == '__main__':
     t = turtle.Turtle()
     t.up()
-    setCordinates()
-    drawBase()
+    initialise_screen()
+    draw_picture_base()
     treeTypes = ['maple','pine']
     treeTypesLength = [150,200]
     maxTreeHeight = 200
@@ -167,16 +178,16 @@ if __name__ == '__main__':
     #noOfTrees = int(input("How many trees in your forest? "))
     noOfTrees = 5
     for _ in range(noOfTrees):
-        tree_height  = drawTree(randint(50,treeTypesLength[_%2]),treeTypes[_%2])
+        tree_height  = draw_tree(randint(50, treeTypesLength[randint(0,1)]), treeTypes[randint(0,1)])
         if(tree_height > maxTreeHeight):
             maxTreeHeight = tree_height
-        t.fd(90)
+        t.fd(100)
 
     #house =  input("Is there a house in the forest (y/n)? ")
     house = 'y'
     if house == 'y':
-        drawHouse(100)
+        draw_house(100)
 
-    drawStar(maxTreeHeight + 80)
+    draw_star(maxTreeHeight + 80)
         
     turtle.done()
